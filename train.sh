a0f993b3c11884c41cae8634e76cf5c1bd2f3ee5
袁建园 <48273390+passengeryjy@users.noreply.github.com>
2022-04-15 11:02:36 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -eux
+
+pretrained_model=./pretrained_lm/bert_base/
+data_dir=./data/DocRED/
+
+lr=5e-5
+epoch=40
+batch_size=4
+
+CUDA_VISIBLE_DEVICES=1 python ./run_docred.py \
+  --model_type bert \
+  --entity_structure biaffine \
+  --model_name_or_path ${pretrained_model} \
+  --do_train \
+  --do_eval \
+  --data_dir ${data_dir} \
+  --max_seq_length 512 \
+  --max_ent_cnt 42 \
+  --per_gpu_train_batch_size ${batch_size} \
+  --learning_rate ${lr} \
+  --num_train_epochs ${epoch} \
+  --warmup_ratio 0.1 \
+  --output_dir checkpoints \
+  --seed 42 \
+  --logging_steps 10
